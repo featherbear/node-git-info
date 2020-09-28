86e1409eea6efc5a4cd6c3bb97400804ced162e3
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-28 17:53:22 +1000

    Add getShaDirty function
    Return abbreviatedSha(-dirty) or null

--- wrapper.js
+++ wrapper.js
@@ -3,9 +3,16 @@ const IsGitDirty = require('is-git-dirty');
 
 // lmao
 
-module.exports = function(path = undefined) {
+const wrap = function(path = undefined) {
   return {
     ...GitRepoInfo(path),
     dirty: IsGitDirty(path)
   }
+}
+
+module.exports = wrap
+module.exports.getShaDirty = function(path = undefined) {
+  const {abbreviatedSha, dirty} = wrap(path)
+  if (!abbreviatedSha) return null;
+  return abbreviatedSha + (dirty ? '-dirty' : '')
 }
